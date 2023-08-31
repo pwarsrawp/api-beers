b117533f288da51aeb38d01a8a47375a69ebf0e8
pwarsrawp <pwarsrawp@gmail.com>
2023-08-31 17:08:16 +0200

    Modified CORS to accept all URLs

--- config/index.js
+++ config/index.js
@@ -22,9 +22,15 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
+  // app.use(
+  //   cors({
+  //     origin: [FRONTEND_URL]
+  //   })
+  // );
+
   app.use(
     cors({
-      origin: [FRONTEND_URL]
+      origin: "*"
     })
   );
 
